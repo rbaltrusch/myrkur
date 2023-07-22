fb076f6041e1431c9b2f7fa67446b970900d13ba
Richard Baltrusch <richard@baltrusch.net>
2023-07-22 12:03:36 +0200

    Add camera player size offset and clean code

--- src/camera.lua
+++ src/camera.lua
@@ -10,22 +10,15 @@ local function floor_epsilon(number)
 end
 
 local function update(self, player, dt)
-    local dist_x = player.x - self.width / 2 - self.total_x
-    local dist_y = player.y - self.height / 2 - self.total_y
+    local dist_x = player.x + player.TILE_SIZE / 2 - self.width / 2 - self.total_x
+    local dist_y = player.y + player.TILE_SIZE / 2 - self.height / 2 - self.total_y
     if dist_x ~= 0 then
         self.x = floor_epsilon(dist_x * self.speed_factor * dt)
-        --local min = dist_x > 0 and 1 or -1
-        --self.x = math.max(min, dist_x * self.speed_factor)
     end
 
     if dist_y ~= 0 then
         self.y = floor_epsilon(dist_y * self.speed_factor * dt)
-        --local min = dist_y > 0 and 1 or -1
-        --self.y = math.max(min, dist_y * self.speed_factor)
     end
-    -- print(self.x, self.y)
-    -- player.x = player.x - self.x
-    -- player.y = player.y - self.y
     self.total_x = self.total_x + self.x
     self.total_y = self.total_y + self.y
 end
